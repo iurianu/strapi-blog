6266ac441da36bc14214217979e6cded6c7df5c2
Iulian Andriescu <iulianandriescu@gmail.com>
2022-05-20 22:33:11 +0300

    'message'

--- .cache/admin/src/plugins.js
+++ .cache/admin/src/plugins.js
@@ -3,8 +3,8 @@ import contentTypeBuilder from '../../../node_modules/@strapi/plugin-content-typ
 import email from '../../../node_modules/@strapi/plugin-email/strapi-admin.js';
 import upload from '../../../node_modules/@strapi/plugin-upload/strapi-admin.js';
 import graphql from '../../../node_modules/@strapi/plugin-graphql/strapi-admin.js';
-import usersPermissions from '../../../node_modules/@strapi/plugin-users-permissions/strapi-admin.js';
 import i18N from '../../../node_modules/@strapi/plugin-i18n/strapi-admin.js';
+import usersPermissions from '../../../node_modules/@strapi/plugin-users-permissions/strapi-admin.js';
 
 
 const plugins = {
@@ -12,8 +12,8 @@ const plugins = {
   'email': email,
   'upload': upload,
   'graphql': graphql,
-  'users-permissions': usersPermissions,
   'i18n': i18N,
+  'users-permissions': usersPermissions,
 };
 
 export default plugins;
